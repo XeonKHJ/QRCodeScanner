b555f183c00d6151a68fc47de7e905664dc1d124
康宏嘉 <redalertkhj@live.cn>
2021-12-18 02:37:49 +0800

    Config dll project.

--- src/WechatQRCodeLib/WechatQRCodeLib/WechatQRCodeLib.cpp
+++ src/WechatQRCodeLib/WechatQRCodeLib/WechatQRCodeLib.cpp
@@ -1,2 +1,17 @@
 #include "pch.h"
 #include "WechatQRCodeLib.h"
+
+WECHATQRCODELIB_API void ReturnSameInt(int a)
+{
+	
+}
+
+WECHATQRCODELIB_API void DetectQRCodePos()
+{
+	
+}
+
+WECHATQRCODELIB_API void ScanQRCode()
+{
+	
+}
